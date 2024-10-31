27a229d884786fc89774dd37627f9b86efa22db9
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-31 14:10:09 +0500

    Add project files.

---
+++ OOPandDesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPandDesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

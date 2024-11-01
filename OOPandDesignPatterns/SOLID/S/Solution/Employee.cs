a6c2a61f4f11034d6c9df1570e36d83c68fe7bf0
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-11-01 15:15:04 +0500

    SOLID: Implement SRP Solution

---
+++ OOPandDesignPatterns/SOLID/S/Solution/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPandDesignPatterns.SOLID.S.Solution
+{
+    internal class Employee
+    {
+        private string Name { get; set; }
+
+        public void GetName()
+        {
+            Console.WriteLine("Employee name: " + Name);
+        }
+    }
+}
---
+++ OOPandDesignPatterns/SOLID/S/Solution/TimeSheetReport.cs
@@ -0,0 +1,16 @@
+﻿/*
+  Solve the problem by moving the behavior related to printing
+ timesheet reports into a separate class. This change lets you
+ move other report-related stuff to the new class.
+ */
+namespace OOPandDesignPatterns.SOLID.S.Solution
+{
+    internal class TimeSheetReport
+    {
+        //Some field...
+        public void Print(Employee employee)
+        {
+            employee.GetName();
+        }
+    }
+}
